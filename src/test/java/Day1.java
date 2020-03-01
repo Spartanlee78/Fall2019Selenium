9e0ca748f54eab41ba7a641663f957ae7226c7bf
Spartanlee78 <spartanlee78@gmail.com>
2020-03-01 11:36:38 -0600

    first Selenium

---
+++ src/test/java/Day1.java
@@ -0,0 +1,14 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1 {
+    public static void main(String[] args) {
+        //setup chromedriver
+        WebDriverManager.chromedriver().setup();
+        //create chromedriver object
+        ChromeDriver driver = new ChromeDriver();
+        //open some website
+        driver.get("http://google.com");
+    }
+
+}
